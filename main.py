4c1ee3f9cccd857cab0f95bf1d675816ade23529
Alex Abimiku <55246666+AlexAbims@users.noreply.github.com>
2023-04-16 15:14:28 +0000

    addeddynamic data

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Abuja, Nigeria',
+  'salary': 'N.100,000'
+}, {
+  'id': 2,
+  'title': 'Database Administrator',
+  'location': 'New York, USA',
+  'salary': '$.10,000'
+}, {
+  'id': 3,
+  'title': 'Front End Developer',
+  'location': 'Nasarawa, Nigeria'
+}, {
+  'id': 4,
+  'title': 'Computer Scientist',
+  'location': 'Abuja, Nigeria',
+  'salary': 'N.10,000'
+}]
 
 
 @app.route("/")
 def hellow_world():
-  return "Helow Alex"
+  return render_template('home.html', jobs=JOBS, company_name='Modern')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
