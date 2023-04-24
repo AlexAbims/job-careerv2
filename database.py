bcdc705e09bc467b4d76cbdf9ca228d21ccbad11
AlexAbims <55246666+AlexAbims@users.noreply.github.com>
2023-04-24 15:49:18 +0000

    added dynamic pages

--- database.py
+++ database.py
@@ -13,5 +13,16 @@ def load_jobs_from_db():
     return jobs_list
       ## for row in result.all():
            ##jobs.append(dict(row))
-                                 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+  
                        
