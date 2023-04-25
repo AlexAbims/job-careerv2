b67f08cdc673d8a4c78a7eabd30f8d918565380e
AlexAbims <55246666+AlexAbims@users.noreply.github.com>
2023-04-25 12:31:18 +0000

    added post data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request 
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -24,6 +24,14 @@ def show_job(id):
     return "Job not Found", 404
   return render_template('jobpage.html', 
                           job=job)
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data=request.args
+  #store data in the Db
+  # send Email
+  #display acknowlodgment
+  return  render_template('application-submitted.html', 
+                          application=data) 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
